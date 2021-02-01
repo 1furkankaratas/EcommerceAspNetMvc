b6ff4c4487e74e684def327fc983120defbd2185
Furkan Karataş <karatas.furkan@outlook.com>
2021-02-02 00:27:19 +0300

    Add profile page ( profile view and edit page)

--- EcommerceAspNetMvc/Controllers/AccountController.cs
+++ EcommerceAspNetMvc/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,7 @@ using EcommerceAspNetMvc.Models;
 
 namespace EcommerceAspNetMvc.Controllers
 {
+    [RoutePrefix("Account")]
     public class AccountController : BaseController
     {
         
@@ -86,7 +88,88 @@ namespace EcommerceAspNetMvc.Controllers
 
         public ActionResult Profil()
         {
-            return View();
+            //TODO: id ile profile erişim kodlanacak
+            var user = CurrentUser();
+
+            if (user==null)
+            {
+               return RedirectToAction("index", "i");
+            }
+
+            ProfilViewModel model = new ProfilViewModel()
+            {
+                Member = user
+            };
+
+            return View(model);
+        }
+
+        [Route("~/Profil/Edit")]
+        public ActionResult ProfilEdit()
+        {
+            var user = CurrentUser();
+
+            if (user == null)
+            {
+                return RedirectToAction("index", "i");
+            }
+
+            ProfilViewModel model = new ProfilViewModel()
+            {
+                Member = user
+            };
+
+            return View(model);
+        }
+
+        [Route("~/Profil/Edit")]
+        [HttpPost]
+        public ActionResult ProfilEdit(ProfilViewModel model,HttpPostedFileBase img)
+        {
+            try
+            {
+                var user = CurrentUser();
+
+                if (user == null)
+                {
+                    return RedirectToAction("index", "i");
+                }
+
+                var cuser = Context.Members.FirstOrDefault(x => x.Id == user.Id);
+                if (cuser!=null)
+                {
+                    cuser.Bio = model.Member.Bio;
+                    cuser.Name = model.Member.Name;
+                    cuser.Surname = model.Member.Surname;
+                    cuser.ModifiedDate = DateTime.Now;
+                    if (img != null)
+                    {
+                        string path = Server.MapPath("~/wwwroot/img/");
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+
+                        string fileName = DateTime.Now.Month + "-" + DateTime.Now.Day + "-" +
+                                          Path.GetFileName(img.FileName);
+                        img.SaveAs(path +fileName );
+                        cuser.ProfileImageName = fileName;
+                    }
+                }
+
+                
+                Context.SaveChanges();
+
+                Session["logonuser"] = cuser;
+            }
+            catch (Exception e)
+            {
+                ViewBag.info = "Bir hata meydana geldi" + " -> " +e.Message;
+                return View(model);
+            }
+
+
+            return RedirectToAction("Profil","Account");
         }
     }
 }
